b1c7934e5160646c14fd8f82a29fe0b30a021b5a
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2026-01-19 19:27:28 -0300

    chore(project): astro dependencies + eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+
+  {
+    rules: {
+      "no-console": "error"
+    }
+  }
+];
